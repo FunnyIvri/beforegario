e84db28f6e1b6ddee62962ba2ac7469e877399f2
FunnyIvri <itamarivri.best@gmail.com>
2023-04-27 18:05:53 +0300

    better gravity and jump!

--- main.py
+++ main.py
@@ -6,56 +6,43 @@ root = pygame.display.set_mode(root_size)
 clock = pygame.time.Clock()
 fpm = 60
 platforms = []
-
+platform_group = pygame.sprite.Group()
 
 class Player:
-    def __init__(self, image, speed, jump_height, start_pos):
-        self.image = image
+    def __init__(self, image, speed, jump_height, gravity, start_pos):
+        self.gravity = gravity
         self.speed = speed
         self.jump_height = jump_height
         self.start_pos = start_pos
-        self.player = pygame.image.load(self.image).convert_alpha()
-        self.player_rect = self.player.get_rect()
-        self.player_rect.x = self.start_pos[0]
-        self.player_rect.y = self.start_pos[1]
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = self.start_pos[0]
+        self.rect.y = self.start_pos[1]
 
     def show(self):
-        root.blit(self.player, self.player_rect)
+        root.blit(self.image, self.rect)
 
     def move(self):
         dx = 0
-        dy = 0
+        dy = self.gravity
         if keyboard.is_pressed("left"):
             dx = -self.speed
         elif keyboard.is_pressed("right"):
             dx = self.speed
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect) and self.rect.bottom <= platform.rect.centery:
+                dy = 0
         if keyboard.is_pressed("up"):
             for platform in platforms:
-                if self.player_rect.colliderect(platform.platform_rect):
+                if self.rect.colliderect(platform.rect):
                     dy = -self.jump_height
+        self.rect.x += dx
+        self.rect.y += dy
 
-        self.player_rect.x += dx
-        self.player_rect.y += dy
 
-    def gravity(self):
-
-        onplatform = False
-        for platform in platforms:
-            if platform.platform_rect.colliderect(player.player_rect):
-                onplatform = True
-                break
-        if not onplatform:
-            player.player_rect.y += 10
-        # print(self.player_rect.y)
-        # if self.player_rect.y > 200:
-        #    self.player_rect.y += -6
-        # elif self.player_rect.y < 200:
-        #    self.player_rect.y += 10
-
-    #
     def walls(self):
 
-        player_rect = self.player_rect
+        player_rect = self.rect
         if player_rect.left <= 0:
             player_rect.x = root_size[0] - 100
 
@@ -63,35 +50,43 @@ class Player:
             player_rect.x = 0
             player_rect.y = player_rect.y - 10
 
-        self.player_rect = player_rect
+        self.rect = player_rect
 
 
-class Platform:
+class Platform(pygame.sprite.Sprite):
     def __init__(self, image, pos, size):
-        self.image = image
-        self.platform = pygame.image.load(self.image).convert_alpha()
-        self.platform = pygame.transform.scale(self.platform, size)
-        self.platform_rect = self.platform.get_rect()
-        self.platform_rect.centerx = pos[0]
-        self.platform_rect.centery = pos[1]
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image).convert_alpha()
+        self.image = pygame.transform.scale(self.image, size)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = pos[0]
+        self.rect.centery = pos[1]
 
     def show(self):
-        root.blit(self.platform, self.platform_rect)
+        root.blit(self.image, self.rect)
 
 
 run = True
 
-player = Player("player.png", 15, 150, [200, 200])
+player = Player("player.png", 15, 150, 10, [200, 200])
+
 platforms.append(Platform("platform.png", [400, 200], [150, 38]))
 platforms.append(Platform("platform.png", [400, 500], [root_size[0], 38]))
+for platform in platforms:
+    platform_group.add(platform)
+
 while run:
     root.fill("black")
+
     player.show()
-    player.gravity()
+   # player.gravity()
     player.move()
     player.walls()
-    for platform in platforms:
-        platform.show()
+    print(platform_group.sprites())
+    print(type(root))
+    platform_group.draw(root)
+    #for platform in platforms:
+    #    platform.show()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
---
+++ sprite.py
@@ -0,0 +1,94 @@
+import pygame
+import keyboard
+
+root_size = [800, 800]
+root = pygame.display.set_mode(root_size)
+clock = pygame.time.Clock()
+fpm = 60
+platform_group = pygame.sprite.Group()
+run = True
+
+
+class Player:
+    def __init__(self, image, speed, jump_height, start_pos):
+        self.image = image
+        self.speed = speed
+        self.jump_height = jump_height
+        self.start_pos = start_pos
+        self.player = pygame.image.load(self.image).convert_alpha()
+        self.player_rect = self.player.get_rect()
+        self.player_rect.x = self.start_pos[0]
+        self.player_rect.y = self.start_pos[1]
+
+    def show(self):
+        root.blit(self.player, self.player_rect)
+
+    def move(self):
+        dx = 0
+        dy = 0
+        if keyboard.is_pressed("left"):
+            dx = -self.speed
+        elif keyboard.is_pressed("right"):
+            dx = self.speed
+        # if keyboard.is_pressed("up"):
+        #    for platform in platforms:
+        #        if self.player_rect.colliderect(platform.rect):
+        #            dy = -self.jump_height
+
+        self.player_rect.x += dx
+        self.player_rect.y += dy
+
+    # def gravity(self):
+    #
+    #    onplatform = False
+    #    for platform in platforms:
+    #        if platform.rect.colliderect(player.player_rect):
+    #            onplatform = True
+    #            break
+    #    if not onplatform:
+    #        player.player_rect.y += 10
+    #    # print(self.player_rect.y)
+    #    # if self.player_rect.y > 200:
+    #    #    self.player_rect.y += -6
+    #    # elif self.player_rect.y < 200:
+    #    #    self.player_rect.y += 10
+
+    #
+    def walls(self):
+
+        player_rect = self.player_rect
+        if player_rect.left <= 0:
+            player_rect.x = root_size[0] - 100
+
+        elif player_rect.right >= root_size[0]:
+            player_rect.x = 0
+            player_rect.y = player_rect.y - 10
+
+        self.player_rect = player_rect
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+player = Player("player.png", 15, 150, [200, 200])
+pl = Platform("platform.png", 400, 400)
+platform_group.add(pl)
+while run:
+    root.fill("black")
+    player.show()
+    player.move()
+    player.walls()
+    platform_group.draw(root)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    clock.tick(fpm)
+    pygame.display.update()
